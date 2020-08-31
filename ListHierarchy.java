ed0b62deb42a30530a0951c5130c4bf4a26f14cb
Smit Panchal <sm414905@dal.ca>
2020-08-31 00:28:51 -0300

    Code for LinkedList from Scratch

---
+++ ListHierarchy.java
@@ -0,0 +1,14 @@
+
+public class ListHierarchy {
+
+	public ListHierarchy( Coin flip ) {
+	}
+	
+	public static boolean add( String key ) {
+		return true;
+	}
+	
+	public static boolean find( String key ) {
+		return true;
+	}
+}
